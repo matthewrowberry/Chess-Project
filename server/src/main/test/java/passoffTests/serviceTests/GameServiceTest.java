9c6262882e483028e015caaf31c6df947f69e54d
matthewrowberry <matthewrowberry@gmail.com>
2024-02-27 23:15:43 -0700

    testing testing again

---
+++ server/src/main/test/java/passoffTests/serviceTests/GameServiceTest.java
@@ -0,0 +1,20 @@
+package passoffTests.serviceTests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameServiceTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void getGames() {
+    }
+
+    @Test
+    void joinGame() {
+    }
+}
