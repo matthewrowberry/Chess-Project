1e206fcac74e86c01ab5ec5f7b7230167157afbf
matthewrowberry <matthewrowberry@gmail.com>
2024-02-28 10:34:35 -0700

    once again

--- server/src/test/java/passoffTests/serviceTests/DbServiceTest.java
+++ server/src/test/java/passoffTests/serviceTests/DbServiceTest.java
@@ -12,9 +12,9 @@ import service.DbService;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DbServiceTest {
-/*
+
     @Test
-    void clear() {
+    void clearTest() {
         MemoryAuthDAO auths = new MemoryAuthDAO();
         MemoryGameDAO games = new MemoryGameDAO();
         MemoryUserDao users = new MemoryUserDao();
@@ -29,5 +29,5 @@ class DbServiceTest {
         assertEquals(auths1,auths);
         assertEquals(games1,games);
         assertEquals(users1,users);
-    }*/
+    }
 }
